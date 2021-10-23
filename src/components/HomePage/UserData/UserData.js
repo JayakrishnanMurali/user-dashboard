7d66a3e279086dd22e242e4664b464441ff22a49
Jayakrishnan M <jayakrishnan1012@gmail.com>
2021-10-23 18:17:46 +0530

    Working on User page

--- src/components/HomePage/UserData/UserData.js
+++ src/components/HomePage/UserData/UserData.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Plus } from "react-feather";
 
 const UserData = () => {
-  return <div>User Data</div>;
+  return (
+    <div className="h-screen ">
+      <div className="px-8 pt-16 items-center flex justify-between">
+        <h1 className="text-4xl font-semibold">Registered Users</h1>
+        <div className="flex mr-60 bg-blue-500  cursor-pointer transition-all duration-300 ease-in-out hover:bg-blue-700 py-3 px-4 items-center justify-center rounded-full">
+          <Plus className="text-white" />
+          <h1 className="text-white text-lg ml-2">Create User</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserData;
