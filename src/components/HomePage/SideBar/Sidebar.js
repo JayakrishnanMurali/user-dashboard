305f52329594654e74e0a8beb342ef419e593dce
Jayakrishnan M <jayakrishnan1012@gmail.com>
2021-10-23 17:58:04 +0530

    Added CopyRight

--- src/components/HomePage/SideBar/Sidebar.js
+++ src/components/HomePage/SideBar/Sidebar.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Grid, RefreshCcw, Settings } from "react-feather";
+import { Grid, LogOut, RefreshCcw, Settings } from "react-feather";
 
 const Sidebar = () => {
   return (
-    <div className="mt-32 mx-8 flex flex-col">
+    <div className="pt-32 px-8 flex flex-col justify-between h-screen">
       <div>
         <div className="flex items-center bg-blue-100 p-3 w-full rounded-xl cursor-pointer    ">
           <Grid className="text-blue-500" />
@@ -21,6 +21,17 @@ const Sidebar = () => {
           <h1 className="ml-2 text-xl text-gray-500 transition-all duration-300 ease-in-out group-hover:text-blue-500 font-medium">
             Settings
           </h1>
+        </div>
+      </div>
+      <div>
+        <div className="flex mb-4 items-center">
+          <LogOut className="text-gray-500" />
+          <h1 className="ml-2 text-xl text-gray-500 font-medium cursor-pointer">
+            Log out
+          </h1>
+        </div>
+        <div className="mb-8">
+          <p className="text-sm text-gray-400">&copy; 2021 botbaba.io</p>
         </div>
       </div>
     </div>
