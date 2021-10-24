254cf0eea03f2648cd61cd62d625af6476ca4bed
Jayakrishnan M <jayakrishnan1012@gmail.com>
2021-10-24 13:37:08 +0530

    working on table

--- src/components/HomePage/UserData/MaterialUITable.js
+++ src/components/HomePage/UserData/MaterialUITable.js
@@ -3,34 +3,32 @@ import { DataGrid } from "@mui/x-data-grid";
 
 const MaterialUITable = () => {
   const columns = [
-    { field: "id", headerName: "ID", width: 130 },
+    { field: "id", headerName: "ID", width: 100 },
+    { field: "avatar", headerName: "Avatar", width: 170 },
     { field: "firstName", headerName: "First name", width: 170 },
     { field: "lastName", headerName: "Last name", width: 170 },
-    {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      width: 120,
-    },
     {
       field: "fullName",
       headerName: "Full name",
       description: "This column has a value getter and is not sortable.",
       sortable: false,
       width: 160,
+      valueGetter: (params) =>
+        `${params.getValue(params.id, "firstName") || ""} ${
+          params.getValue(params.id, "lastName") || ""
+        }`,
     },
+    { field: "email", headerName: "Email", width: 400 },
   ];
 
   const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+    {
+      id: 1,
+      avatar: "Profile.jpg",
+      lastName: "Snow",
+      firstName: "Jon",
+      email: "jon@gmail.com",
+    },
   ];
 
   return (
@@ -38,7 +36,7 @@ const MaterialUITable = () => {
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={5}
+        pageSize={9}
         rowsPerPageOptions={[5]}
         checkboxSelection
       />
