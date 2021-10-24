8254163ce73fbddbf5ad79fca3d61f70b47ee19f
Jayakrishnan M <jayakrishnan1012@gmail.com>
2021-10-24 12:15:33 +0530

    added table

--- src/components/HomePage/UserData/UserData.js
+++ src/components/HomePage/UserData/UserData.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Plus } from "react-feather";
+import MaterialUITable from "./MaterialUITable";
 
 const UserData = () => {
   return (
     <div className="h-screen ">
       <div className="px-8 pt-16 items-center flex justify-between">
         <h1 className="text-4xl font-semibold">Registered Users</h1>
-        <div className="flex mr-60 bg-blue-500  cursor-pointer transition-all duration-300 ease-in-out hover:bg-blue-700 py-3 px-4 items-center justify-center rounded-full">
+        <div className="flex mr-60 bg-blue-500 cursor-pointer transition-all duration-300 ease-in-out hover:bg-blue-700 py-3 px-4 items-center justify-center rounded-full">
           <Plus className="text-white" />
           <h1 className="text-white text-lg ml-2">Create User</h1>
         </div>
       </div>
+
+      <MaterialUITable />
     </div>
   );
 };
